b64204a3d6bc813b2800428f3e2a86834ff3601e
wltjq57 <wltjq57@naver.com>
2024-08-21 16:04:18 +0900

    상속(오버라이드) - 삼각형 넓이

---
+++ src/intermediateJava/FigureInheritDemo.java
@@ -0,0 +1,32 @@
+package intermediateJava;
+
+public class FigureInheritDemo {
+  public static void main(String[] args) {
+    Triangle triangle = new Triangle(3, 3);
+    double area = triangle.getArea();
+    System.out.println(area);
+  }
+}
+
+class Figure {
+  private int poly;
+  public double getArea(){
+    return 0.0;
+  }
+}
+
+class Triangle extends Figure {
+  private int poly = 3;
+  private int height;
+  private int width;
+
+  public Triangle(int height, int width) { // 생성자
+    this.height = height;
+    this.width = width;
+  }
+
+  @Override // 컨트롤 + O = 오버라이드
+  public double getArea() {
+    return (width * height) / (double)2;
+  }
+}

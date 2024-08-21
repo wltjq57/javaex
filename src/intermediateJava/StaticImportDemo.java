5a6abc2246e7ba9a33ef4fa5eeaae0843d238576
wltjq57 <wltjq57@naver.com>
2024-08-21 16:04:50 +0900

    패키지 정적import

---
+++ src/intermediateJava/StaticImportDemo.java
@@ -0,0 +1,11 @@
+package intermediateJava;
+
+import java.util.Calendar;
+import static java.util.Calendar.getInstance;
+
+public class StaticImportDemo {
+  public static void main(String[] args) {
+    getInstance();
+
+  }
+}

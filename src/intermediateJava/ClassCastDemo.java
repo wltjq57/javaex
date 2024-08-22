bebfa56724e402e47552d8b34426fef5803c8300
wltjq57 <wltjq57@naver.com>
2024-08-22 10:49:36 +0900

    상속과 접근제어(public)

--- src/intermediateJava/ClassCastDemo.java
+++ src/intermediateJava/ClassCastDemo.java
@@ -3,7 +3,7 @@ package intermediateJava;
 public class ClassCastDemo {
   public static void main(String[] args) {
 
-    Triangle triangle = new Triangle(3, 3);
+    Triangle triangle = new Triangle(3, 3,3);
     Figure f = triangle;
     System.out.println(f.poly);
   }
